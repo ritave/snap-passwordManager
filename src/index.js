fec70e12030b873d1df539d842fef968c648c09f
ritave <olaf.tomalka@gmail.com>
2022-04-19 13:49:40 +0200

    Created the password manager snap

--- src/index.js
+++ src/index.js
@@ -1,18 +1,69 @@
+import { Mutex } from 'async-mutex';
+
+function fuzzySearch(haystack, needle) {
+  let regexPattern = '.*';
+  for (let i = 0; i < needle.length; i++) {
+    regexPattern += needle[i] + '.*';
+  }
+  const regex = new RegExp(regexPattern);
+
+  return haystack.reduce((results, possibillity) => {
+    if (possibillity.search(regex) !== -1) {
+      results.push(possibillity);
+    }
+    return results;
+  }, []);
+}
+
+const saveMutext = new Mutex();
+
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
+  const state =
+    (await wallet.request({ method: 'snap_manageState', params: ['get'] })) ??
+    {};
+
+  let website, username, password;
   switch (requestObject.method) {
-    case 'hello':
-      return wallet.request({
+    case 'save_password':
+      ({ website, username, password } = requestObject);
+      await saveMutext.runExclusive(async () => {
+        const oldState =
+          (await wallet.request({
+            method: 'snap_manageState',
+            params: ['get'],
+          })) ?? {};
+        const newState = {
+          ...oldState,
+          [website]: { username, password },
+        };
+        await wallet.request({
+          method: 'snap_manageState',
+          params: ['update', newState],
+        });
+      });
+      return 'OK';
+    case 'get_password':
+      ({ website } = requestObject);
+      const showPassword = await wallet.request({
         method: 'snap_confirm',
         params: [
           {
-            prompt: `Hello, ${originString}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+            prompt: 'Confirm password request?',
+            description: 'Do you want to display the password in plaintext?',
+            textAreaContent: `The DApp "${originString}" is asking to display the account and password for "${website}" website`,
           },
         ],
       });
+      if (!showPassword) {
+        return undefined;
+      }
+      return state[website];
+    case 'search':
+      const { pattern } = requestObject;
+      return fuzzySearch(Object.keys(state), pattern);
+    case 'clear':
+      await wallet.request({ method: 'snap_manageState', params: ['clear'] });
+      return 'OK';
     default:
       throw new Error('Method not found.');
   }
